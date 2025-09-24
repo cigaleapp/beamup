e1454f53d32f47b081f933568562afbe0806b2f3
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-09-24 11:44:47 +0200

    ➖ Remove dotenv
    Bun loads .env files natively

--- src/database.ts
+++ src/database.ts
@@ -1,7 +1,5 @@
 import { Database } from 'bun:sqlite';
 // oxlint-disable-next-line import/extensions
-import 'dotenv/config';
-// oxlint-disable-next-line import/extensions
 import { drizzle } from 'drizzle-orm/bun-sqlite';
 
 export const db = drizzle(new Database(Bun.env.DB_FILE_NAME));
