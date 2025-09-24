d97ea47713bddabef4c14100e6133f84e63ed877
Gwenn Le Bihan <gwenn.lebihan7@gmail.com>
2025-09-24 18:02:53 +0200

    🏷️ Add override keyword for CorsedResponse class

--- src/cors.ts
+++ src/cors.ts
@@ -4,14 +4,14 @@ export class CorsedResponse extends Response {
 		this.headers.set('Access-Control-Allow-Origin', Bun.env.ALLOWED_ORIGINS || '*');
 	}
 
-	static json(body: any, init?: ResponseInit) {
+	static override json(body: any, init?: ResponseInit) {
 		const response = super.json(body, init);
 		if (!response.headers.has('Access-Control-Allow-Origin'))
 			response.headers.set('Access-Control-Allow-Origin', Bun.env.ALLOWED_ORIGINS || '*');
 		return response;
 	}
 
-	static redirect(url: string, status?: number) {
+	static override redirect(url: string, status?: number) {
 		const response = super.redirect(url, status);
 		if (!response.headers.has('Access-Control-Allow-Origin'))
 			response.headers.set('Access-Control-Allow-Origin', Bun.env.ALLOWED_ORIGINS || '*');
